1d37fd80ead4bec2137d49586c879eced1a2b472
Eduardo Ridao <manoguar@msn.com>
2023-03-30 20:04:31 +0200

    removed dummy classes

--- Engine/src/NI2S.Node.Core.Abstractions/Framework/Player/PlayerInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using Orleans;
-using System;
-
-namespace NI2S.Node.Framework
-{
-    [GenerateSerializer, Immutable]
-    public record class PlayerInfo(Guid Key, string Name) : ActorInfo(Key, Name);
-}
