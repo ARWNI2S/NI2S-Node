2214f48e12e4907c5fce5559944fad84dfbb981a
Eduardo Ridao <manoguar@msn.com>
2023-03-30 13:06:36 +0200

    deleee

---
+++ Engine/test/TestNode/Program.cs
@@ -0,0 +1,18 @@
+﻿// TODO: find and replace this line with COPYRIGTH NOTICE entire solution
+
+using Microsoft.Extensions.Hosting;
+
+namespace TestNode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var builder = NodeHost.CreateHostBuilder(args);
+            var builder2 = Host.CreateApplicationBuilder(args);
+            //builder.UseOrleans(siloBuilder => siloBuilder.UseLocalhostClustering())
+            //var app = builder.Build();
+            var app2 = builder2.Build();
+        }
+    }
+}
