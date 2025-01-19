3e98c614919131191bd7b3110435df487f01c92f
Eduardo Ridao <manoguar@msn.com>
2025-01-19 21:59:28 +0100

    full/local build pipeline :P (hope it works)

---
+++ src/ARWNI2S.Node.Orleans/ActorProxy.cs
@@ -0,0 +1,9 @@
+﻿using ARWNI2S.Core;
+using Orleans;
+
+namespace ARWNI2S.Node.Orleans
+{
+    internal class ActorProxy : Grain<PersistentState>, INiisActorProxy
+    {
+    }
+}
---
+++ src/ARWNI2S.Node.Orleans/PersistentState.cs
@@ -0,0 +1,6 @@
+﻿namespace ARWNI2S.Node.Orleans
+{
+    internal class PersistentState
+    {
+    }
+}
